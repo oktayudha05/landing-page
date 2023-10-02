83a161b0b9ca6631cc09a296b3b2bd7f9eb2957f
oktayudha05 <oktayudha05@gmail.com>
2023-10-02 14:46:03 +0700

    change class to className

--- frontend/src/utils/login.jsx
+++ frontend/src/utils/login.jsx
@@ -1,5 +1,5 @@
-import Signup from "./createAccount.jsx"
-import { useNavigate, BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import { useNavigate} from "react-router-dom"
 
 function Login() {
 
@@ -14,40 +14,40 @@ function Login() {
 
 
     return (
-        <section class="bg-gray-50 dark:bg-white-500">
-  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <a href="/" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-black">
-      <img src="/img/Logo FAV.jpg" alt="" style={{width:50}} class="pr-2"/>
+        <section className="bg-gray-50 dark:bg-white-500">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <a href="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-black">
+      <img src="/img/Logo FAV.jpg" alt="" style={{width:50}} className="pr-2"/>
           URFAV
       </a>
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-400 dark:border-gray-950">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-black">
+      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-400 dark:border-gray-950">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-black">
                   Sign in to your account
               </h1>
-              <form class="space-y-4 md:space-y-6" action="#">
+              <form className="space-y-4 md:space-y-6" action="#">
                   <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your email</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email@untidar.ac.id" required=""/>
+                      <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your email</label>
+                      <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email@untidar.ac.id" required=""/>
                   </div>
                   <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-black">Password</label>
-                      <input type="password" name="password" id="password" placeholder="Your Password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                      <label htmlFor="password" className="block mb-2 text-sm font-medium text-black">Password</label>
+                      <input type="password" name="password" id="password" placeholder="Your Password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                   </div>
-                  <div class="flex items-center justify-between">
-                      <div class="flex items-start">
-                          <div class="flex items-center h-5">
-                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
+                  <div className="flex items-center justify-between">
+                      <div className="flex items-start">
+                          <div className="flex items-center h-5">
+                            <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
                           </div>
-                          <div class="ml-3 text-sm">
-                            <label for="remember" class="text-black">Remember me</label>
+                          <div className="ml-3 text-sm">
+                            <label htmlFor="remember" className="text-black">Remember me</label>
                           </div>
                       </div>
-                      <a href="#" class="text-gray-400 hover:underline text-black-400">Forgot password?</a>
+                      <a href="#" className="text-gray-400 hover:underline text-black-400">Forgot password?</a>
                   </div>
-                  <button type="submit" class="text-white bg-black hover:bg-slate-50 hover:text-black focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 ">Sign in</button>
-                  <p class="text-sm font-light text-black ">
-                      Don’t have an account yet? <a href="" onClick={() => navigate('/signup')} class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                  <button type="submit" className="text-white bg-black hover:bg-slate-50 hover:text-black focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 ">Sign in</button>
+                  <p className="text-sm font-light text-black ">
+                      Don’t have an account yet? <a href="" onClick={() => navigate('/signup')} className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                   </p>
               </form>
               
