cf8bbfaf64ae0c6c8236e1c29a7dc26938983be6
fzlfade <faizal210505@gmail.com>
2023-10-02 13:24:14 +0700

    bg change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./utils/navbar"
 import Login from "./utils/login"
-
+import Signup from "./utils/createAccount"
 
 const App = () =>  {
 
@@ -10,6 +10,7 @@ const App = () =>  {
       <Routes>
         <Route path="/" element={<Navbar/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
       </Routes>
     </Router>
   )
--- src/utils/createAccount.jsx
+++ src/utils/createAccount.jsx
@@ -1,20 +1,18 @@
 function createAccount() {
     return(
         <>
-            <div className="container bg-dark text-center mb-5">
-                <h1 className="text-light">Buat akun dlu bang</h1>
-            <div className="mb-3 row text-light">
-                <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Email</label>
-                <div className="col-sm-10">
-                    <input type="text" className="form-control" id="staticEmail"></input>
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Buat akun dlu bang</h1>
+                <div>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email@untidar.ac.id" required=""/>
                 </div>
-                </div>
-                <div className="mb-3 row text-light">
-                    <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
-                    <div className="col-sm-10">
-                        <input type="password" className="form-control" id="inputPassword"></input>
-                    </div>
-                </div>
+                <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="password" name="password" id="password" placeholder="Your Password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                  </div>
                 <div className="d-flex flex-row-reverse">
                     <button type="submit" className="btn btn-primary mb-3 col-3">Submit</button>
                     <div className="container text-start mt-2">
@@ -22,6 +20,8 @@ function createAccount() {
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
         </>
     )
 }
